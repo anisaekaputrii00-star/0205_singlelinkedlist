03404cb967751593be5164686a6a8de5fd92aa93
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-21 11:17:14 +0700

    masukan nodeBaru

--- Singlinklist.cpp
+++ Singlinklist.cpp
@@ -8,3 +8,39 @@ class node
     int noMhs;
     Node *next;
 };
+
+class LinkedList
+{
+    Node *START;
+    public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node();
+        nodeBaru->noMhs = nim;
+
+
+        
+
+        if (START == NULL || START->noMhs >= nodeBaru->noMhs)
+        {
+            if (START != NULL && START->noMhs == nodeBaru->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+        
+        {
+            
+        }
+        cout << endl;
+    }
+};
