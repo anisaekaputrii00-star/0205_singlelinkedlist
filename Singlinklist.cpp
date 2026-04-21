3a10392622134bf07790a0564c6183eba26a329b
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-21 11:32:05 +0700

    menambahkan fungsi search

--- Singlinklist.cpp
+++ Singlinklist.cpp
@@ -57,7 +57,23 @@ class LinkedList
           
         }
         
-            
+        bool listEmpty()
+        {
+            return START == NULL;
+        }   
+        
+        bool search(int nim, Node *&previous, Node *&current)
+        {
+            previous = START;
+            current = START;
+
+            while (current != NULL && current->noMhs != nim)
+            {
+                previous = current;
+                current = current->next;
+            }
+            return current != NULL;
+        }
         
         
     }
